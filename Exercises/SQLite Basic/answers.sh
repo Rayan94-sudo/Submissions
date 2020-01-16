4f05c870a40f99290c9880363ed8ce739bed6b00
Rayan <rayan@localhost.localdomain>
2020-01-16 22:15:11 +0200

    Creating Table

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -4,3 +4,15 @@ select name,points from students where name = "Alex";
 insert into students(name,age,gender,points) values("Rayan","25","F","30");
 update students set points = points +1 where name="Basma";
 update students set points = points -1 where name="Alex";
+
+CREATE TABLE `graduates` (
+	`Id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`Name`	TEXT NOT NULL UNIQUE,
+	`Age`	INTEGER,
+	`Gender`	TEXT,
+	`Points`	INTEGER,
+	`Graduation`	TEXT )
+
+insert into Graduates(Name, Age, Gender, Points)  select Name, Age, Gender, Points from students where Name="Layal";
+update Graduates set graduation ="10/10/2010" where Name="Layal";
+delete from Graduates where name="Layal";
