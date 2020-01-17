d7d023c194e8e46bd92bacf2d83062fc2e4368d4
Rayan <rayan@localhost.localdomain>
2020-01-17 16:36:50 +0200

    change

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -1,5 +1,5 @@
 select name from students;
-select age from students where age > 30;
+select age,name from students where gender="f",age="30";
 select name,points from students where name = "Alex";
 insert into students(name,age,gender,points) values("Rayan","25","F","30");
 update students set points = points +1 where name="Basma";
