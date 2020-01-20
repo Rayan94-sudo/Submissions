60e71293a81e2d89d2f3b12d54a2d6fe8091c1ff
Rayan <rayan@localhost.localdomain>
2020-01-20 11:26:26 +0200

    handled empty state

--- Challenge/NodeJS/tasks.js
+++ Challenge/NodeJS/tasks.js
@@ -33,19 +33,21 @@ function startApp(name) {
  */
 function onDataReceived(text) {
   var x = text.split(" ");
-  console.log(x[0]);
   console.log("..........");
   if (text === "quit\n" || text === "exit\n") {
     quit();
-  } else if (x[0] == "hello") {
-    helloo(x[1]);
+  } else if (text === "hello\n") {
+    hello();
   } else {
-    if (text === "help\n") {
-      help();
-    } else unknownCommand(text);
+    if (x[0] == "hello") {
+      helloo(x[1]);
+    } else {
+      if (text === "help\n") {
+        help();
+      } else unknownCommand(text);
+    }
   }
 }
-
 /**
  * help function
  * print choises of user
