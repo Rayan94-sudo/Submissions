9aa08bbb46cfe51de04431d295e43f919694fc09
Rayan <rayan@localhost.localdomain>
2020-01-21 15:25:27 +0200

    SQL change

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -1,28 +1,17 @@
-select name from students;
-select age,name from students where gender="f",age="30";
-select name,points from students where name = "Alex";
-insert into students(name,age,gender,points) values("Rayan","25","F","30");
-update students set points = points +1 where name="Basma";
-update students set points = points -1 where name="Alex";
-
-CREATE TABLE `graduates` (
-	`Id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`Name`	TEXT NOT NULL UNIQUE,
-	`Age`	INTEGER,
-	`Gender`	TEXT,
-	`Points`	INTEGER,
-	`Graduation`	TEXT )
-
-insert into Graduates(Name, Age, Gender, Points)  select Name, Age, Gender, Points from students where Name="Layal";
-update Graduates set graduation ="10/10/2010" where Name="Layal";
-delete from Graduates where name="Layal";
-
-select employees.Name,companies.Name,companies.Date from employees,companies;
-select employees.Name,companies.Date from employees,companies where companies.date<2000;
-select companies.Name,employees.Role  from companies,employees where employees.role="Graphic Designer";
-
-select Name from (select Name, max(points) from students);
-select avg(points) from students;
-select count(students.ID) from students where Points=500;
-select students.Name from students where students.Name like '%s%';
-select * from students order by points asc;
+"select name from students;"
+"select age,name from students where gender="f",age="30";"
+"select name,points from students where name = "Alex";"
+"insert into students(name,age,gender,points) values("Rayan","25","F","30");"
+"update students set points = points +1 where name="Basma";"
+"update students set points = points -1 where name="Alex";"
+"insert into Graduates(Name, Age, Gender, Points)  select Name, Age, Gender, Points from students where Name="Layal";"
+"update Graduates set graduation ="10/10/2010" where Name="Layal";"
+"delete from Graduates where name="Layal";"
+"select employees.Name,companies.Name,companies.Date from employees,companies;"
+"select employees.Name,companies.Date from employees,companies where companies.date<2000;"
+"select companies.Name,employees.Role  from companies,employees where employees.role="Graphic Designer";"
+"select Name from (select Name, max(points) from students);"
+"select avg(points) from students;"
+"select count(students.ID) from students where Points=500;"
+"select students.Name from students where students.Name like '%s%';"
+"select * from students order by points asc;"
